60440723ea72e2c7f337757136fbb6a44cfe4e41
Aditya Nugraha <adityanugraha795@gmail.com>
2021-02-06 22:22:00 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Daftar Nama Angkatan SMAN 1 Gorontalo</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel = "stylesheet" type="text/css" href="asset/bootstrap.min.css" />
+		
+		<script src="asset/jquery.slim.min.js"></script>
+		<script src="asset/bootstrap.bundle.min.js"></script>
+	</head>
+<body>
+<!-- Page Content -->
+<div class="container">
+
+  <center>  <div class="col-lg-3 col-md-4 col-6">
+      <a href="https://www.instagram.com/ikasmansagorontalo/" class="d-block mb-4 h-100">
+	  <p> IKA SMANSA GORONTALO</p>
+          <img class="img-fluid img-thumbnail" src="img/ikasmansa.png" alt=""> 
+          </a>
+    </div></center>
+
+  <hr class="mt-2 mb-5">
+
+  <div class="row text-center text-lg-left">
+
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="https://www.instagram.com/akkadia14/" class="d-block mb-4 h-100">
+	  <p> Akkadia'14</p>
+            <img class="img-fluid img-thumbnail" src="img/akkadia.jpg" alt="">
+          </a>
+    </div>
+	<div class="col-lg-3 col-md-4 col-6">
+      <a href="#" class="d-block mb-4 h-100">
+	  <p> Immortal'15</p>
+            <img class="img-fluid img-thumbnail" src="img/immortal.jpg" alt="">
+          </a>
+    </div>
+   <div class="col-lg-3 col-md-4 col-6">
+      <a href="https://www.instagram.com/centurionsmansa/" class="d-block mb-4 h-100">
+	  <p> Centurion'16</p>
+            <img class="img-fluid img-thumbnail" src="img/centurion.jpg" alt="">
+          </a>
+    </div>
+	<div class="col-lg-3 col-md-4 col-6">
+      <a href="https://www.instagram.com/anamorphic_17/" class="d-block mb-4 h-100">
+	  <p> Anamorphic'17</p>
+            <img class="img-fluid img-thumbnail" src="img/anamorphic.jpg" alt="">
+          </a>
+    </div>
+	<div class="col-lg-3 col-md-4 col-6">
+      <a href="https://www.instagram.com/majestic_smansa/" class="d-block mb-4 h-100">
+	  <p> Majestic'18</p>
+            <img class="img-fluid img-thumbnail" src="img/majestic.jpg" alt="">
+          </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="https://www.instagram.com/asterium.smansa/" class="d-block mb-4 h-100">
+	  <p> Asterium'19</p>
+            <img class="img-fluid img-thumbnail" src="img/asterium.jpg" alt="">
+          </a>
+    </div>
+	<div class="col-lg-3 col-md-4 col-6">
+      <a href="https://www.instagram.com/aesthetic.smansa/" class="d-block mb-4 h-100">
+	  <p> Aesthetic'20</p>
+            <img class="img-fluid img-thumbnail" src="img/aesthetic.jpg" alt="">
+          </a>
+    </div>
+
+
+
+
+</div>
+<h1 class="font-weight-light text-center text-lg-left mt-4 mb-0"><center>Who Is Next ?</center></h1> 
+<!-- /.container -->
+<div class="footer">
+
+    <font color="black"> Copyright &copy; 1951 - <?=date('Y'); ?><a href="http://sma1gorontalo.sch.id" target="_blank"> SMA NEGERI 1 GORONTALO</a>
+
+  </div>
+</body>
+</html>
